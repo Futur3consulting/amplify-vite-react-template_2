1406a5f77a76bb369be24ea3891ed98af3779f38
Futur3consulting <clint.armstrong@futur3consulting.com>
2025-03-23 14:43:22 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   return (
   <Authenticator>
     {({ signOut, user }) => (
-    <main>
+     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>Add New</button>
       <ul>
